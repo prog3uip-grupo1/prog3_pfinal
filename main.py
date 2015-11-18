f2b9e38d4a7bf44f8c30e4b10af06371e4bf32d9
Julio Lerma <jlerma27@hotmail.com>
2015-11-18 11:18:17 -0500

    Test Kivy

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Proyecto Final 2")
+__version__ = "1.0"
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+
+
+class MainWindow(BoxLayout):
+    pass
+
+
+class PFinalApp(App):
+    def build(self):
+        return MainWindow()
+
+if __name__ == "__main__":
+    PFinalApp().run()
