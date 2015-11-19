9f796197cfe269163c1ab325c53e5d8114a99931
Julio Lerma <jlerma27@hotmail.com>
2015-11-19 08:24:35 -0500

    Test in Pycharm with python 2.7

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 __version__ = "1.0"
 from kivy.app import App
+from kivy.properties import StringProperty
 from kivy.uix.boxlayout import BoxLayout
 
 
 class MainWindow(BoxLayout):
-    texto = "Proyecto Final Test"
+    texto = StringProperty("Proyecto Final Test")
 
 
 class PFinalApp(App):
